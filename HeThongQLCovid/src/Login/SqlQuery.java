ccd901444d6759a43a0eb846289ef0e7e340213f
Nghia-Hieu <63239928+Nghia-Hieu@users.noreply.github.com>
2021-11-23 11:04:54 +0700

    Connect MSSQL

--- HeThongQLCovid/src/Login/SqlQuery.java
+++ HeThongQLCovid/src/Login/SqlQuery.java
@@ -7,10 +7,14 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SqlQuery {
+	private static 	String dbURL = "jdbc:sqlserver://NIERA-ASUS\\SQLEXPRESS; databaseName=HeThongQuanLyCovid",
+			user = "sa",
+			pass = "12345678";
+
 	public static ResultSet checkLogin (Account account) throws ClassNotFoundException, SQLException {
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Covid","root","hieunghia3107");
+		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		Connection conn = DriverManager.getConnection(dbURL, user, pass);
 		Statement st = conn.createStatement();
-		return st.executeQuery("SELECT * FROM patient WHERE ID= '"+account.getUsername()+"'AND Password='"+account.getPassword()+"'");
+		return st.executeQuery("SELECT * FROM ql_user WHERE username= '"+account.getUsername()+"'AND user_password='"+account.getPassword()+"'");
 	}
 }
