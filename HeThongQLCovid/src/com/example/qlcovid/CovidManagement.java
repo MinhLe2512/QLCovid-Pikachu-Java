f07b0b009af6d03d876a436310985667b360d0fc
MinhLe2512 <63902542+MinhLe2512@users.noreply.github.com>
2021-11-24 21:26:40 +0700

    thêm hd trong CovidManagement.java

--- HeThongQLCovid/src/com/example/qlcovid/CovidManagement.java
+++ HeThongQLCovid/src/com/example/qlcovid/CovidManagement.java
@@ -1,6 +1,12 @@
 package com.example.qlcovid;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import com.example.qlcovid.jframe.UserFrame;
+import com.example.qlcovid.jframe.ViewInfoFrame;
+import com.example.qlcovid.model.UserAccount;
 
 import Login.LoginFrame;
 
@@ -12,10 +18,36 @@ public class CovidManagement {
 				try {
 					LoginFrame window = new LoginFrame();
 					//window.frame.setVisible(true);
-					//new window then login here
+					//new window then login handlecases 
+					//cứ làm vầy vô handlecases new LoginFrame
+					//LoginFrame nhập vô username, password
+					//Check cái đó xong rồi ok thì duyệt qua xong rồi check role rồi check validation
+					//xong rồi new account thích hợp
+					//nếu new cái user account thì nhớ dùng cái string sqlSelect t để trong package string
+					//để tiện khi mà user gọi view info mình ko phải check database nữa 
+					handleCases();
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
+			}
+		});
+	}
+	public static void handleCases() {
+		//if 1, 2, 3 gì gì đó xong rồi thì giải quyết từng chức năng
+		//t demo cái user thôi
+		UserAccount uAcc = new UserAccount();
+		UserFrame uFrame = new UserFrame();
+		//Click button show basic info
+		uFrame.btnViewInfo.addActionListener(new ActionListener() {		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ViewInfoFrame vFrame = new ViewInfoFrame();
+				vFrame.setCid(uAcc.getCitizenId());
+				vFrame.setFullname(uAcc.getFullName());
+				vFrame.setDob(uAcc.getDateOfBirth());
+				vFrame.setAddress(uAcc.getAddress());
+				vFrame.setsetCondition(uAcc.getCondition());
 			}
 		});
 	}
